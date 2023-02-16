9da8dd5da7bd8877011e3030c6f2ae746ac421e2
Alessio Limina <alessio.limina@gmail.com>
2023-02-16 10:42:25 +0100

    Updated Exercise Stream 02 with Javadoc and Try/Catch Statement

--- src/it/develhope/streams/Start.java
+++ src/it/develhope/streams/Start.java
@@ -16,35 +16,42 @@ public class Start {
     public static void main(String[] args) throws IOException {
 
         /**
-        * * Creating a new File, specifying the pathname;
-        * if the file already exists, the program will inform the user about overwrite it;
-        * Creating a new BufferedWriter, to use in combination with FileWriter to write a file
-        */
-
-        File file = new File("D:\\Documenti Alessio\\Develhope\\Corso Java\\GIT\\Streams 02\\src\\file.txt");
-        if (file.exists()) {System.out.println("Alert: the file already exists, I'll overwrite it");
-        } else {System.out.println("Roger! New file created!");}
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,false));
-
-        /**
-         * Creating the current date/time with seconds
-         * creating a new DateTimeFormatter
-         * Formatting the current date/time in a more readable pattern using dateTimeFormatter.ofPattern
+         * Creating a new File, specifying the pathname;
+         * if the file already exists, the program will inform the user about overwrite it;
+         * Creating a new BufferedWriter, to use in combination with FileWriter to write a file
          */
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss");
+        try {
+            File file = new File("D:\\Documenti Alessio\\Develhope\\Corso Java\\GIT\\Streams 02\\src\\file.txt");
+            if (file.exists()) {
+                System.out.println("Alert: the file already exists, I'll overwrite it");
+            } else {
+                System.out.println("Roger! New file created!");
+            }
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, false));
 
-        /**
-         * Create a String time that contains the formatted date/time
-         * write the String time in the new .txt file
-         */
+            /**
+             * Creating the current date/time with seconds
+             * creating a new DateTimeFormatter
+             * Formatting the current date/time in a more readable pattern using dateTimeFormatter.ofPattern
+             */
 
-        String time = localDateTime.format(dateTimeFormatter);
-        bufferedWriter.write("The current date/time is " + time);
-        bufferedWriter.close();
+            LocalDateTime localDateTime = LocalDateTime.now();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss");
 
+            /**
+             * Create a String time that contains the formatted date/time
+             * write the String time in the new .txt file
+             */
+
+            String time = localDateTime.format(dateTimeFormatter);
+            bufferedWriter.write("The current date/time is " + time);
+            bufferedWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+}
 
 
